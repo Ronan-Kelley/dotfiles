9fa862f73c27b31a5aaf8faf08bd0534d7f863dd
Ronan Kelley <Ronan-Kelley@users.noreply.github.com>
2020-04-26 02:17:02 -0400

    initial upload

---
+++ collect.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+# simple script to copy relevant dotfiles into this directory
+#
+
+
+# for ease of use. follows links, acts recursively, doesn't overwrite up-to-date files, and prints
+# verbosely.
+copy='cp -L -r -u -v'
+
+### ensure subdirs exist (github doesn't upload empty dirs, hoping to reduce maintenance on this script) ###
+mkdir -p ./.config/i3
+mkdir -p ./.config/nvim
+
+### i3 configs ###
+$copy ~/.config/i3/* ./.config/i3/
+
+### neovim configs ###
+$copy ~/.config/nvim/* ./.config/nvim/
+$copy ~/.vim ./.vim
+
+### bash configs ###
+$copy ~/.bashrc ./.bashrc
+$copy ~/.bash_profile ./.bash_profile
